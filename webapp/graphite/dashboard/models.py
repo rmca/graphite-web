35159cb54d73d7af250f513b5eceb93e1638947a
Dave Concannon <dave@metricfire.com>
2012-10-10 12:03:51 -0700

    Dashboard slugification, first commit of two

--- webapp/graphite/dashboard/models.py
+++ webapp/graphite/dashboard/models.py
@@ -5,7 +5,7 @@ from graphite.account.models import Profile
 
 class Dashboard(models.Model):
   class Admin: pass
-  name = models.CharField(primary_key=True, max_length=128)
+  name = models.CharField(max_length=128)
   owners = models.ManyToManyField(Profile, related_name='dashboards')
   state = models.TextField()
   __str__ = lambda self: "Dashboard [%s]" % self.name
