50e008e7a693170d2819415aa35d6cbc787cf0ad
Dave Concannon <dave@metricfire.com>
2013-01-17 16:57:23 -0800

    Allow share url to include underscores

--- webapp/content/js/dashboard.js
+++ webapp/content/js/dashboard.js
@@ -2444,7 +2444,7 @@ function deleteDashboard(name) {
 }
 
 function slugify(text) {
-	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+	text = text.replace(/[^_-a-zA-Z0-9,&\s]+/ig, '');
 	text = text.replace(/-/gi, "_");
 	text = text.replace(/\s/gi, "-");
 	return text.toLowerCase();
